fd5e684e2026c09d651e31c2a83e7efc311ae45a
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:17:51 +0530

    Update variables.tf

--- terraform/modules/gcp/dashboards/variables.tf
+++ terraform/modules/gcp/dashboards/variables.tf
@@ -1,2 +1,9 @@
-variable "name_prefix" {}
-variable "suffix" {}
+variable "name_prefix" {
+  description = "Prefix for dashboard name"
+  type        = string
+}
+
+variable "suffix" {
+  description = "Unique suffix for resource name"
+  type        = string
+}
