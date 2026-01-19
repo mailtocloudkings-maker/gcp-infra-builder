6f3d5dc16c1c91f22acd56cf50b74c8bee411e91
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 22:52:58 +0530

    Update backend.tf

--- terraform/environments/non-prod/gcp/backend.tf
+++ terraform/environments/non-prod/gcp/backend.tf
@@ -1,6 +1,8 @@
 terraform {
+  required_version = ">= 1.3"
+  
   backend "gcs" {
-    bucket  = var.tf_state_bucket
-    prefix  = var.tf_state_prefix
+    bucket = var.tf_state_bucket
+    prefix = var.tf_state_prefix
   }
 }
