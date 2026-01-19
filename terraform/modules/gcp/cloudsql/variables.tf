a4236dcb31a8b9c1430f5f0d1b1726ef340ec571
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:33:27 +0530

    Update variables.tf

--- terraform/modules/gcp/cloudsql/variables.tf
+++ terraform/modules/gcp/cloudsql/variables.tf
@@ -1,3 +1,21 @@
-variable "network_id" {}
-variable "name_prefix" {}
-variable "suffix" {}
+variable "name_prefix" {
+  description = "Prefix for CloudSQL instance"
+  type        = string
+}
+
+variable "suffix" {
+  description = "Unique suffix for the instance"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for CloudSQL"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "tier" {
+  description = "Machine type tier for CloudSQL"
+  type        = string
+  default     = "db-f1-micro"
+}
