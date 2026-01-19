9852ca9540172dc1a72ba9e50ccf79bc1386ef88
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 00:42:11 +0530

    Update outputs.tf

--- terraform/modules/gcp/compute-vm/outputs.tf
+++ terraform/modules/gcp/compute-vm/outputs.tf
@@ -12,3 +12,19 @@ output "vm_zone" {
   description = "Zone where VM is deployed"
   value       = google_compute_instance.vm.zone
 }
+
+# Optional: CloudSQL connection info passed to VM
+output "cloudsql_private_ip" {
+  description = "CloudSQL private IP attached to VM"
+  value       = var.cloudsql_private_ip
+}
+
+output "cloudsql_user" {
+  description = "CloudSQL username configured on VM"
+  value       = var.cloudsql_user
+}
+
+output "cloudsql_db_name" {
+  description = "CloudSQL database name configured on VM"
+  value       = var.cloudsql_db_name
+}
