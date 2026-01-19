43e2fdcadab1db62b9a9d12bd6601f84673ea850
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:43:48 +0530

    Update main.tf

--- terraform/modules/gcp/dashboards/main.tf
+++ terraform/modules/gcp/dashboards/main.tf
@@ -1,6 +1,3 @@
-variable "name_prefix" {}
-variable "suffix" {}
-
 resource "google_monitoring_dashboard" "this" {
   dashboard_json = jsonencode({
     displayName = "${var.name_prefix}-dashboard-${var.suffix}"
