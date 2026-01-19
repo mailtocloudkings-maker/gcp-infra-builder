3100eab1cd16b59e19d4ab51821c1778ce723fe8
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 00:51:45 +0530

    Update outputs.tf

--- terraform/modules/gcp/compute-vm/outputs.tf
+++ terraform/modules/gcp/compute-vm/outputs.tf
@@ -1,30 +1,35 @@
+# -----------------------------
+# VM Outputs
+# -----------------------------
 output "vm_name" {
-  description = "The name of the VM"
+  description = "The name of the VM instance"
   value       = google_compute_instance.vm.name
 }
 
 output "vm_self_link" {
-  description = "Self link of the VM"
+  description = "Self link of the VM instance"
   value       = google_compute_instance.vm.self_link
 }
 
 output "vm_zone" {
-  description = "Zone where VM is deployed"
+  description = "The zone where the VM is deployed"
   value       = google_compute_instance.vm.zone
 }
 
-# Optional: CloudSQL connection info passed to VM
+# -----------------------------
+# CloudSQL Connection Info (Optional)
+# -----------------------------
 output "cloudsql_private_ip" {
-  description = "CloudSQL private IP attached to VM"
-  value       = var.cloudsql_private_ip
+  description = "Private IP of the CloudSQL instance attached to the VM (if provided)"
+  value       = length(var.cloudsql_private_ip) > 0 ? var.cloudsql_private_ip : null
 }
 
 output "cloudsql_user" {
-  description = "CloudSQL username configured on VM"
-  value       = var.cloudsql_user
+  description = "CloudSQL username configured on the VM (if provided)"
+  value       = length(var.cloudsql_user) > 0 ? var.cloudsql_user : null
 }
 
 output "cloudsql_db_name" {
-  description = "CloudSQL database name configured on VM"
-  value       = var.cloudsql_db_name
+  description = "CloudSQL database name configured on the VM (if provided)"
+  value       = length(var.cloudsql_db_name) > 0 ? var.cloudsql_db_name : null
 }
