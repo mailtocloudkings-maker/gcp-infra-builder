0eca376d661e92d1c10ecb192224a026772906b0
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 22:53:18 +0530

    Update variables.tf

--- terraform/environments/non-prod/gcp/variables.tf
+++ terraform/environments/non-prod/gcp/variables.tf
@@ -1,38 +1,18 @@
-variable "project_id" {
-  type = string
-}
+variable "create_vpc" { type = bool }
+variable "create_firewall" { type = bool }
+variable "create_dns" { type = bool }
+variable "create_compute_vm" { type = bool }
+variable "create_compute_mig" { type = bool }
+variable "create_ilb" { type = bool }
+variable "create_cloudsql" { type = bool }
+variable "create_storage" { type = bool }
+variable "create_monitoring" { type = bool }
+variable "create_alerts" { type = bool }
+variable "create_dashboards" { type = bool }
 
-variable "create_vpc" {
-  type    = bool
-  default = false
-}
+variable "tf_state_bucket" { type = string }
+variable "tf_state_prefix" { type = string }
 
-variable "create_firewall" {
-  type    = bool
-  default = true
-}
-
-variable "create_dns_internal" {
-  type    = bool
-  default = false
-}
-
-variable "create_compute_vm" {
-  type    = bool
-  default = false
-}
-
-variable "create_cloudsql_postgres" {
-  type    = bool
-  default = false
-}
-
-variable "create_storage" {
-  type    = bool
-  default = false
-}
-
-variable "create_monitoring" {
-  type    = bool
-  default = false
-}
+variable "project_id" { type = string }
+variable "region" { type = string }
+variable "zone" { type = string }
