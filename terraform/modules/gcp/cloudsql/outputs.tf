5d4aa5389710ff466209b838170cd01a151f2558
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:34:11 +0530

    Update outputs.tf

--- terraform/modules/gcp/cloudsql/outputs.tf
+++ terraform/modules/gcp/cloudsql/outputs.tf
@@ -1,1 +1,14 @@
-output "instance_name" { value = google_sql_database_instance.postgres.name }
+output "instance_name" {
+  value       = google_sql_database_instance.postgres.name
+  description = "Name of the CloudSQL instance"
+}
+
+output "private_ip_address" {
+  value       = google_sql_database_instance.postgres.private_ip_address
+  description = "Private IP of CloudSQL instance"
+}
+
+output "db_name" {
+  value       = google_sql_database.default_db.name
+  description = "Default database name"
+}
