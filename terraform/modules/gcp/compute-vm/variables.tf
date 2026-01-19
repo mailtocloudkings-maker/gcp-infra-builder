576b1d3a403ecd34078a0d86e4848ce21d371bf5
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:38:52 +0530

    Update variables.tf

--- terraform/modules/gcp/compute-vm/variables.tf
+++ terraform/modules/gcp/compute-vm/variables.tf
@@ -1,55 +1,11 @@
-variable "name_prefix" {
-  description = "Prefix for VM names"
-  type        = string
-}
+variable "name_prefix" { type = string }
+variable "suffix" { type = string }
+variable "subnet_id" { type = string }
+variable "zone" { type = string }
+variable "machine_type" { type = string }
+variable "image" { type = string }
+variable "tags" { type = list(string), default = [] }
 
-variable "suffix" {
-  description = "Unique suffix for VM names"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnetwork ID for the VM"
-  type        = string
-}
-
-variable "zone" {
-  description = "Zone where VM will be created"
-  type        = string
-}
-
-variable "machine_type" {
-  description = "GCE machine type"
-  type        = string
-  default     = "e2-micro"
-}
-
-variable "image" {
-  description = "VM boot image"
-  type        = string
-  default     = "debian-cloud/debian-11"
-}
-
-variable "tags" {
-  description = "Network tags for the VM"
-  type        = list(string)
-  default     = []
-}
-
-variable "cloudsql_private_ip" {
-  description = "CloudSQL private IP to connect from VM"
-  type        = string
-  default     = ""
-}
-
-variable "cloudsql_user" {
-  description = "CloudSQL default user for VM metadata"
-  type        = string
-  default     = ""
-}
-
-variable "cloudsql_db_name" {
-  description = "CloudSQL default DB name for VM metadata"
-  type        = string
-  default     = ""
-}
+variable "cloudsql_private_ip" { type = string, default = "" }
+variable "cloudsql_user" { type = string, default = "" }
+variable "cloudsql_db_name" { type = string, default = "" }
