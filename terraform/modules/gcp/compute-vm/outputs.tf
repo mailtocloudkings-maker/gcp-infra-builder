c10eddb4cbe14a8813053354aa4393c0ff0e156f
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:32:08 +0530

    Update outputs.tf

--- terraform/modules/gcp/compute-vm/outputs.tf
+++ terraform/modules/gcp/compute-vm/outputs.tf
@@ -1,3 +1,14 @@
 output "vm_name" {
-  value = google_compute_instance.vm.name
+  description = "The name of the VM"
+  value       = google_compute_instance.vm.name
+}
+
+output "vm_self_link" {
+  description = "Self link of the VM"
+  value       = google_compute_instance.vm.self_link
+}
+
+output "vm_zone" {
+  description = "Zone where VM is deployed"
+  value       = google_compute_instance.vm.zone
 }
