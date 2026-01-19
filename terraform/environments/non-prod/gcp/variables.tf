c0abbab80a8fd44d255d15b3845235da642a0596
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 00:12:03 +0530

    Update variables.tf

--- terraform/environments/non-prod/gcp/variables.tf
+++ terraform/environments/non-prod/gcp/variables.tf
@@ -1,18 +1,66 @@
-variable "create_vpc" { type = bool }
-variable "create_firewall" { type = bool }
-variable "create_dns_internal" { type = bool }
-variable "create_compute_vm" { type = bool }
-variable "create_compute_mig" { type = bool }
-variable "create_ilb" { type = bool }
-variable "create_cloudsql" { type = bool }
-variable "create_storage" { type = bool }
-variable "create_monitoring" { type = bool }
-variable "create_alerts" { type = bool }
-variable "create_dashboards" { type = bool }
+# ✅ Only resources that we actually create
+variable "create_firewall" {
+  type = bool
+  description = "Create firewall rules"
+}
 
-variable "tf_state_bucket" { type = string }
-variable "tf_state_prefix" { type = string }
+variable "create_dns_internal" {
+  type = bool
+  description = "Create internal DNS records"
+}
 
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "zone" { type = string }
+variable "create_compute_vm" {
+  type = bool
+  description = "Create a Compute VM"
+}
+
+variable "create_compute_mig" {
+  type = bool
+  description = "Create a Managed Instance Group"
+}
+
+variable "create_ilb" {
+  type = bool
+  description = "Create an Internal Load Balancer"
+}
+
+variable "create_cloudsql" {
+  type = bool
+  description = "Create CloudSQL PostgreSQL instance"
+}
+
+variable "create_storage" {
+  type = bool
+  description = "Create Storage Bucket"
+}
+
+variable "create_monitoring" {
+  type = bool
+  description = "Enable Monitoring"
+}
+
+variable "create_alerts" {
+  type = bool
+  description = "Create Alerts"
+}
+
+variable "create_dashboards" {
+  type = bool
+  description = "Create Dashboards"
+}
+
+# GCP project info
+variable "project_id" {
+  type = string
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  type = string
+  description = "GCP Region"
+}
+
+variable "zone" {
+  type = string
+  description = "GCP Zone"
+}
