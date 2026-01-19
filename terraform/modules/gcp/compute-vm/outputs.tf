864b836782370c90e44a16f181e90f9c89806f7d
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:20:03 +0530

    Update outputs.tf

--- terraform/modules/gcp/compute-vm/outputs.tf
+++ terraform/modules/gcp/compute-vm/outputs.tf
@@ -1,35 +1,34 @@
-# -----------------------------
-# VM Outputs
-# -----------------------------
+# ==============================
+# VM Outputs (conditional)
+# ==============================
+
 output "vm_name" {
-  description = "The name of the VM instance"
-  value       = google_compute_instance.vm.name
+  description = "The name of the VM"
+  value       = var.create_compute_vm && length(module.compute_vm) > 0 ? module.compute_vm[0].vm_name : ""
 }
 
 output "vm_self_link" {
-  description = "Self link of the VM instance"
-  value       = google_compute_instance.vm.self_link
+  description = "The self link of the VM"
+  value       = var.create_compute_vm && length(module.compute_vm) > 0 ? module.compute_vm[0].vm_self_link : ""
 }
 
 output "vm_zone" {
-  description = "The zone where the VM is deployed"
-  value       = google_compute_instance.vm.zone
+  description = "Zone where the VM is deployed"
+  value       = var.create_compute_vm && length(module.compute_vm) > 0 ? module.compute_vm[0].vm_zone : ""
 }
 
-# -----------------------------
-# CloudSQL Connection Info (Optional)
-# -----------------------------
-output "cloudsql_private_ip" {
-  description = "Private IP of the CloudSQL instance attached to the VM (if provided)"
-  value       = length(var.cloudsql_private_ip) > 0 ? var.cloudsql_private_ip : null
+# Optional: CloudSQL info passed to VM
+output "vm_cloudsql_private_ip" {
+  description = "CloudSQL private IP attached to VM"
+  value       = var.create_compute_vm && length(module.compute_vm) > 0 ? module.compute_vm[0].cloudsql_private_ip : ""
 }
 
-output "cloudsql_user" {
-  description = "CloudSQL username configured on the VM (if provided)"
-  value       = length(var.cloudsql_user) > 0 ? var.cloudsql_user : null
+output "vm_cloudsql_user" {
+  description = "CloudSQL username configured on VM"
+  value       = var.create_compute_vm && length(module.compute_vm) > 0 ? module.compute_vm[0].cloudsql_user : ""
 }
 
-output "cloudsql_db_name" {
-  description = "CloudSQL database name configured on the VM (if provided)"
-  value       = length(var.cloudsql_db_name) > 0 ? var.cloudsql_db_name : null
+output "vm_cloudsql_db_name" {
+  description = "CloudSQL database name configured on VM"
+  value       = var.create_compute_vm && length(module.compute_vm) > 0 ? module.compute_vm[0].cloudsql_db_name : ""
 }
