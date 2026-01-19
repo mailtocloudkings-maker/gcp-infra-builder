4f32f4c36c0f707cf985b6518887e446928779b1
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:23:50 +0530

    Update outputs.tf

--- terraform/modules/gcp/cloudsql/outputs.tf
+++ terraform/modules/gcp/cloudsql/outputs.tf
@@ -1,28 +1,19 @@
-# ==============================
-# CloudSQL Outputs (conditional)
-# ==============================
-
-output "cloudsql_instance_name" {
-  description = "CloudSQL instance name"
-  value       = var.create_cloudsql && length(module.cloudsql) > 0 ? module.cloudsql[0].instance_name : ""
+output "instance_name" {
+  value = google_sql_database_instance.postgres.name
 }
 
-output "cloudsql_connection_name" {
-  description = "CloudSQL instance connection string"
-  value       = var.create_cloudsql && length(module.cloudsql) > 0 ? module.cloudsql[0].instance_connection_name : ""
+output "instance_connection_name" {
+  value = google_sql_database_instance.postgres.connection_name
 }
 
-output "cloudsql_private_ip" {
-  description = "CloudSQL private IP"
-  value       = var.create_cloudsql && length(module.cloudsql) > 0 ? module.cloudsql[0].private_ip : ""
+output "private_ip" {
+  value = google_sql_database_instance.postgres.private_ip_address
 }
 
-output "cloudsql_db_name" {
-  description = "Default database name in CloudSQL"
-  value       = var.create_cloudsql && length(module.cloudsql) > 0 ? module.cloudsql[0].default_db_name : ""
+output "default_db_name" {
+  value = var.create_default_db ? google_sql_database.default_db[0].name : ""
 }
 
-output "cloudsql_user" {
-  description = "Default CloudSQL username"
-  value       = var.create_cloudsql && length(module.cloudsql) > 0 ? module.cloudsql[0].default_user_name : ""
+output "default_user_name" {
+  value = var.create_default_user ? google_sql_user.default_user[0].name : ""
 }
