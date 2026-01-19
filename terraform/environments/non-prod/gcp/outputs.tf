0980f76ac9fa00c3f6eb41f022513e5d6207b730
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:37:41 +0530

    Update outputs.tf

--- terraform/environments/non-prod/gcp/outputs.tf
+++ terraform/environments/non-prod/gcp/outputs.tf
@@ -1,22 +1,21 @@
+# Outputs
 # -----------------------------
-# Outputs for pipeline / BindPlane
-# -----------------------------
-output "vm_name" {
-  value       = module.compute_vm[0].vm_name
-  description = "First VM name"
-}
-
 output "cloudsql_private_ip" {
-  value       = module.cloudsql[0].private_ip
-  description = "CloudSQL private IP"
+  value = var.create_cloudsql ? module.cloudsql[0].private_ip : ""
 }
 
 output "cloudsql_user" {
-  value       = module.cloudsql[0].default_user_name
-  description = "CloudSQL default user"
+  value = var.create_cloudsql ? module.cloudsql[0].default_user_name : ""
 }
 
 output "cloudsql_db_name" {
-  value       = module.cloudsql[0].default_db_name
-  description = "CloudSQL default database"
+  value = var.create_cloudsql ? module.cloudsql[0].default_db_name : ""
+}
+
+output "vm_name" {
+  value = var.create_compute_vm ? module.compute_vm[0].vm_name : ""
+}
+
+output "vm_zone" {
+  value = var.create_compute_vm ? module.compute_vm[0].vm_zone : ""
 }
