520ada2bdfd21bd75758df4970d2f7988b9450a9
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:30:38 +0530

    Update variables.tf

--- terraform/modules/gcp/compute-vm/variables.tf
+++ terraform/modules/gcp/compute-vm/variables.tf
@@ -1,4 +1,17 @@
-variable "subnet_id" { type = string }
-variable "name_prefix" { type = string }
-variable "suffix" { type = string }
-variable "tags" { type = list(string) }
+# Subnet to attach the VM to
+variable "subnet_id" {
+  description = "ID of the subnet where VM will be deployed"
+  type        = string
+}
+
+# Prefix for VM name
+variable "name_prefix" {
+  description = "Prefix for the VM name"
+  type        = string
+}
+
+# Unique suffix for VM name
+variable "suffix" {
+  description = "Unique suffix for the VM name"
+  type        = string
+}
