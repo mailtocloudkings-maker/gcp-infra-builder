b9012e22da89153d051c11e5e67209cd8105be03
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:18:08 +0530

    Update outputs.tf

--- terraform/modules/gcp/dashboards/outputs.tf
+++ terraform/modules/gcp/dashboards/outputs.tf
@@ -1,1 +1,4 @@
-output "dashboard_name" { value = google_monitoring_dashboard.this.dashboard_json }
+output "dashboard_name" {
+  value       = google_monitoring_dashboard.this.dashboard_json
+  description = "JSON configuration of the dashboard"
+}
