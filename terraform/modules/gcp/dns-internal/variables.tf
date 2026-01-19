47e79ecb62ac84cd21767e3c6ea3624df07e4d86
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:01:38 +0530

    Create variables.tf

---
+++ terraform/modules/gcp/dns-internal/variables.tf
@@ -0,0 +1,24 @@
+# The name prefix for DNS zone names
+variable "name_prefix" {
+  description = "Prefix for DNS zone names"
+  type        = string
+}
+
+# Unique suffix for resource names
+variable "suffix" {
+  description = "Unique suffix for DNS zone names"
+  type        = string
+}
+
+# The VPC network ID where the DNS zone will be attached
+variable "network_id" {
+  description = "VPC network ID for internal DNS"
+  type        = string
+}
+
+# Domain name for internal DNS zone (e.g., internal.example.com)
+variable "domain_name" {
+  description = "Domain name for the internal DNS zone"
+  type        = string
+  default     = "internal.local"
+}
