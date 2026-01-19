de92629c9f187a3b9caedf5a3fcbe1cafa7f8401
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:45:26 +0530

    Update variables.tf

--- terraform/modules/gcp/compute-vm/variables.tf
+++ terraform/modules/gcp/compute-vm/variables.tf
@@ -1,11 +1,55 @@
-variable "name_prefix" { type = string }
-variable "suffix" { type = string }
-variable "subnet_id" { type = string }
-variable "zone" { type = string }
-variable "machine_type" { type = string }
-variable "image" { type = string }
-variable "tags" { type = list(string), default = [] }
+variable "name_prefix" {
+  description = "Prefix for VM instance names"
+  type        = string
+}
 
-variable "cloudsql_private_ip" { type = string, default = "" }
-variable "cloudsql_user" { type = string, default = "" }
-variable "cloudsql_db_name" { type = string, default = "" }
+variable "suffix" {
+  description = "Unique suffix for VM instance names"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the VM"
+  type        = string
+}
+
+variable "zone" {
+  description = "GCP zone for the VM"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM"
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "image" {
+  description = "Compute image for the VM"
+  type        = string
+  default     = "debian-cloud/debian-11"
+}
+
+variable "tags" {
+  description = "List of tags to assign to VM"
+  type        = list(string)
+  default     = []
+}
+
+variable "cloudsql_private_ip" {
+  description = "Private IP of CloudSQL to connect from VM"
+  type        = string
+  default     = ""
+}
+
+variable "cloudsql_user" {
+  description = "CloudSQL username to connect from VM"
+  type        = string
+  default     = ""
+}
+
+variable "cloudsql_db_name" {
+  description = "CloudSQL database name to connect from VM"
+  type        = string
+  default     = ""
+}
