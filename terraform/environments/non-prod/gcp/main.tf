dd866a211c82cb335a7f4dc668d63ac94268165c
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:37:17 +0530

    Update main.tf

--- terraform/environments/non-prod/gcp/main.tf
+++ terraform/environments/non-prod/gcp/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     google = {
@@ -17,7 +16,7 @@ provider "google" {
   zone    = var.zone
 }
 
-# Generate unique suffix for resource names
+# Generate unique suffix
 resource "random_id" "suffix" {
   byte_length = 3
 }
@@ -37,26 +36,12 @@ data "google_compute_subnetwork" "default_subnet" {
   region = var.region
 }
 
-# -----------------------------
-# Firewall Module
-# -----------------------------
-module "firewall" {
-  count       = var.create_firewall ? 1 : 0
-  source      = "../../../modules/gcp/firewall"
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-}
-
-# -----------------------------
-# Enable Service Networking
-# -----------------------------
+# Enable Service Networking for CloudSQL
 resource "google_project_service" "service_networking" {
   service = "servicenetworking.googleapis.com"
 }
 
-# -----------------------------
-# Reserve IP range for CloudSQL
-# -----------------------------
+# Reserve private IP range for CloudSQL
 resource "google_compute_global_address" "private_ip_range" {
   name          = "cloudsql-private-range"
   purpose       = "VPC_PEERING"
@@ -65,9 +50,7 @@ resource "google_compute_global_address" "private_ip_range" {
   network       = data.google_compute_network.default_vpc.id
 }
 
-# -----------------------------
 # Create Service Networking connection
-# -----------------------------
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = data.google_compute_network.default_vpc.id
   service                 = "servicenetworking.googleapis.com"
@@ -77,7 +60,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 }
 
 # -----------------------------
-# CloudSQL Postgres Module
+# CloudSQL PostgreSQL Module
 # -----------------------------
 module "cloudsql" {
   count       = var.create_cloudsql ? 1 : 0
@@ -88,13 +71,10 @@ module "cloudsql" {
   region                 = var.region
   network_id             = data.google_compute_network.default_vpc.id
   default_user_password  = var.cloudsql_default_user_password
-  create_default_db      = true
-  default_db_name        = "appdb"
-  create_default_user    = true
-  default_user_name      = "appuser"
 
   depends_on = [google_service_networking_connection.private_vpc_connection]
 }
+
 # -----------------------------
 # Compute VM Module
 # -----------------------------
@@ -102,102 +82,16 @@ module "compute_vm" {
   count       = var.create_compute_vm ? 1 : 0
   source      = "../../../modules/gcp/compute-vm"
 
-  name_prefix          = local.name_prefix
-  suffix               = local.unique_suffix
-  subnet_id            = data.google_compute_subnetwork.default_subnet.id
-  zone                 = var.zone
-  machine_type         = "e2-micro"                 # make sure this is a variable in compute-vm module
-  image                = "debian-cloud/debian-11"  # make sure this is a variable in compute-vm module
-  tags                 = ["vm"]
+  name_prefix         = local.name_prefix
+  suffix              = local.unique_suffix
+  subnet_id           = data.google_compute_subnetwork.default_subnet.id
+  zone                = var.zone
+  machine_type        = "e2-micro"
+  image               = "debian-cloud/debian-11"
+  tags                = ["vm"]
 
-  cloudsql_private_ip  = var.create_cloudsql ? module.cloudsql[0].private_ip : ""
-  cloudsql_user        = var.create_cloudsql ? module.cloudsql[0].default_user_name : ""
-  cloudsql_db_name     = var.create_cloudsql ? module.cloudsql[0].default_db_name : ""
-}
-
-
-# -----------------------------
-# Compute MIG Module
-# -----------------------------
-module "compute_mig" {
-  count       = var.create_compute_mig ? 1 : 0
-  source      = "../../../modules/gcp/compute-mig"
-  subnet_id   = data.google_compute_subnetwork.default_subnet.id
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-}
-
-# -----------------------------
-# Internal Load Balancer Module
-# -----------------------------
-module "ilb" {
-  count       = var.create_ilb ? 1 : 0
-  source      = "../../../modules/gcp/load-balancer/internal"
-  network_id  = data.google_compute_network.default_vpc.id
-  subnet_id   = data.google_compute_subnetwork.default_subnet.id
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-}
-
-# -----------------------------
-# Internal DNS Module
-# -----------------------------
-module "dns_internal" {
-  count       = var.create_dns_internal && var.create_ilb ? 1 : 0
-  source      = "../../../modules/gcp/dns-internal"
-  network_id  = data.google_compute_network.default_vpc.id
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-  domain_name = "internal.nonprod.example.com"
-
-  records = [
-    {
-      name    = "internal-lb"
-      type    = "A"
-      ttl     = 300
-      rrdatas = var.create_ilb ? [module.ilb[0].lb_ip] : []
-    }
-  ]
-
-  depends_on = [module.ilb]
-}
-
-# -----------------------------
-# Storage Bucket Module
-# -----------------------------
-module "storage" {
-  count       = var.create_storage ? 1 : 0
-  source      = "../../../modules/gcp/storage-bucket"
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-}
-
-# -----------------------------
-# Monitoring Module
-# -----------------------------
-module "monitoring" {
-  count       = var.create_monitoring ? 1 : 0
-  source      = "../../../modules/gcp/monitoring"
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-}
-
-# -----------------------------
-# Alerts Module
-# -----------------------------
-module "alerts" {
-  count       = var.create_alerts ? 1 : 0
-  source      = "../../../modules/gcp/alerts"
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
-}
-
-# -----------------------------
-# Dashboards Module
-# -----------------------------
-module "dashboards" {
-  count       = var.create_dashboards ? 1 : 0
-  source      = "../../../modules/gcp/dashboards"
-  name_prefix = local.name_prefix
-  suffix      = local.unique_suffix
+  # CloudSQL info for VM
+  cloudsql_private_ip = var.create_cloudsql ? module.cloudsql[0].private_ip : ""
+  cloudsql_user       = var.create_cloudsql ? module.cloudsql[0].default_user_name : ""
+  cloudsql_db_name    = var.create_cloudsql ? module.cloudsql[0].default_db_name : ""
 }
