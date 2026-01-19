03f3f9a70e937ef516ad52255709d38284cc1a83
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:39:07 +0530

    Update main.tf

--- terraform/modules/gcp/compute-vm/main.tf
+++ terraform/modules/gcp/compute-vm/main.tf
@@ -1,6 +1,5 @@
 resource "google_compute_instance" "vm" {
-  count        = var.create_vm ? 1 : 0
-  name         = "${var.name_prefix}-vm-${var.suffix}"
+  name         = "${var.name_prefix}-${var.suffix}"
   machine_type = var.machine_type
   zone         = var.zone
 
@@ -12,13 +11,8 @@ resource "google_compute_instance" "vm" {
 
   network_interface {
     subnetwork = var.subnet_id
-    access_config {}
-  }
-
-  metadata = {
-    cloudsql_private_ip = var.cloudsql_private_ip
-    cloudsql_user       = var.cloudsql_user
-    cloudsql_db_name    = var.cloudsql_db_name
+    # optionally connect to CloudSQL private IP
+    network_ip = var.cloudsql_private_ip != "" ? var.cloudsql_private_ip : null
   }
 
   tags = var.tags
