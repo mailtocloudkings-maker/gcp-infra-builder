14a2cf694c2b279ff12899101f2c28c075da0fd9
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 02:39:19 +0530

    Update outputs.tf

--- terraform/modules/gcp/compute-vm/outputs.tf
+++ terraform/modules/gcp/compute-vm/outputs.tf
@@ -1,23 +1,19 @@
 output "vm_name" {
-  value = var.create_vm && length(google_compute_instance.vm) > 0 ? google_compute_instance.vm[0].name : ""
-}
-
-output "vm_self_link" {
-  value = var.create_vm && length(google_compute_instance.vm) > 0 ? google_compute_instance.vm[0].self_link : ""
+  value = google_compute_instance.vm.name
 }
 
 output "vm_zone" {
-  value = var.create_vm && length(google_compute_instance.vm) > 0 ? google_compute_instance.vm[0].zone : ""
+  value = google_compute_instance.vm.zone
 }
 
 output "cloudsql_private_ip" {
-  value = var.create_vm && length(google_compute_instance.vm) > 0 ? google_compute_instance.vm[0].metadata["cloudsql_private_ip"] : ""
+  value = var.cloudsql_private_ip
 }
 
 output "cloudsql_user" {
-  value = var.create_vm && length(google_compute_instance.vm) > 0 ? google_compute_instance.vm[0].metadata["cloudsql_user"] : ""
+  value = var.cloudsql_user
 }
 
 output "cloudsql_db_name" {
-  value = var.create_vm && length(google_compute_instance.vm) > 0 ? google_compute_instance.vm[0].metadata["cloudsql_db_name"] : ""
+  value = var.cloudsql_db_name
 }
