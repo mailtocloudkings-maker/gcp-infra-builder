17a55058dd91a5e1ade86f0466c2f56d69bdc504
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 00:32:16 +0530

    Update main.tf

--- terraform/modules/gcp/cloudsql/main.tf
+++ terraform/modules/gcp/cloudsql/main.tf
@@ -3,24 +3,38 @@ data "google_compute_network" "default_vpc" {
   name = "default"
 }
 
+# Generate unique CloudSQL instance name
+locals {
+  instance_name = "${var.name_prefix}-pgsql-${var.suffix}"
+}
+
 # CloudSQL instance
 resource "google_sql_database_instance" "postgres" {
-  name             = "${var.name_prefix}-pgsql-${var.suffix}"
+  name             = local.instance_name
   database_version = "POSTGRES_14"
   region           = var.region
 
   settings {
     tier = var.tier
+
     ip_configuration {
       # Attach to default VPC for private IP
       private_network = data.google_compute_network.default_vpc.id
     }
+
+    # Optional: enable automated backups
+    backup_configuration {
+      enabled = var.enable_backups
+    }
   }
+
+  deletion_protection = var.deletion_protection
 }
 
 # Optional: Create a default database
 resource "google_sql_database" "default_db" {
-  name     = "appdb"
+  count    = var.create_default_db ? 1 : 0
+  name     = var.default_db_name
   instance = google_sql_database_instance.postgres.name
   charset  = "UTF8"
   collation = "en_US.UTF8"
@@ -28,7 +42,8 @@ resource "google_sql_database" "default_db" {
 
 # Optional: Create a user
 resource "google_sql_user" "default_user" {
-  name     = "appuser"
+  count    = var.create_default_user ? 1 : 0
+  name     = var.default_user_name
   instance = google_sql_database_instance.postgres.name
-  password = "P@ssword123"  # Replace with secret management in prod
+  password = var.default_user_password
 }
