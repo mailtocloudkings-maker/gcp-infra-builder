b98d061509244bfc69868c0b546b8c592b532686
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-19 23:28:55 +0530

    Update variables.tf

--- terraform/modules/gcp/firewall/variables.tf
+++ terraform/modules/gcp/firewall/variables.tf
@@ -1,14 +1,9 @@
-variable "network_id" {
-  description = "ID of the VPC network"
-  type        = string
-}
-
 variable "name_prefix" {
   description = "Prefix for firewall name"
   type        = string
 }
 
 variable "suffix" {
-  description = "Unique suffix for resource name"
+  description = "Unique suffix for firewall name"
   type        = string
 }
